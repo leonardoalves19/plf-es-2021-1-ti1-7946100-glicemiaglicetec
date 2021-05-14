5073e985bc112a88900737cae138489bb7080cf3
IPI Meu Mini Artigo de Reprodução <82840508+NicoChabot@users.noreply.github.com>
2021-05-14 02:14:11 -0300

    Create databaselogin.js

---
+++ Codigo/databaselogin.js
@@ -0,0 +1,11 @@
+$(function() {
+    var user = document.getElementById(usuario);
+    if (val() == 0)
+        alert(Usuario_inexistente);
+    else
+        var user = document.getElementById(senha);
+    if (val() == 0)
+        alert(Senha_incorreta)
+    else
+        return "#btnEntrar";
+})
